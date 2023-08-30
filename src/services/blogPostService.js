5e7e9d87fb98059c2e09bb0d06d25a15a37cd26e
Paloma Wink <paloma.wink@hotmail.com>
2023-08-30 14:47:23 -0300

    Requisito 13 concluido

--- src/services/blogPostService.js
+++ src/services/blogPostService.js
@@ -1,7 +1,12 @@
-const { BlogPost } = require('../models');
+const { BlogPost, User, Category } = require('../models');
 
 const getAllPosts = async () => {
-  const allPosts = await BlogPost.findAll();
+  const allPosts = await BlogPost.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories' },
+    ],
+  });
   return { status: 200, data: allPosts };
 };
 
